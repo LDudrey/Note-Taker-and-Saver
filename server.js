ecddde40eb149923f6fd0c54b8fd823c06f2e796
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-12-24 00:04:49 -0600

    added GET API for notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 
@@ -10,13 +11,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Returns index.html
+// GET Route for index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Returns notes.html
+// GET Route for notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
+// GET API Route for notes that reads the db.json file and returns all saved notes.
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
