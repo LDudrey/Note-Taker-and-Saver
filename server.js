c312526c39fe24148b1dac021ba74a606a3e65d5
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-12-23 23:58:26 -0600

    added GET for html files

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware function to parse incoming requests
+// Middleware functions
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Returns index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Returns notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
