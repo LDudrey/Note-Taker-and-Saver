f565e785e28395ab8ebee29bd97e281957438a70
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-12-23 23:45:11 -0600

    added urlencoded middleware

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Middleware function to parse incoming requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
