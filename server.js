708183fa5500b122184b8467a6bed2c3ca2407d7
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-12-23 23:41:31 -0600

    added port syntax for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
